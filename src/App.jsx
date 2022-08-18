0e007eeb47bbf135b766c0801cc603f29d901a73
uletin <dokterkoding111@gmail.com>
2022-08-18 15:39:42 +0800

    functional tes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import create from 'zustand'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const useStore = create(set => ({
+  count : 1,
+  increments : () => set(state => ({count : state.count + 1}))
+}))
+
+// const useStore = create(
+//   function (set) {
+//     return {
+//       count : 1,
+//       increments : function () {
+//         set(
+//           function (state) {
+//             return {
+//               count:state.count+1
+//             }
+//           }
+//         )
+//       }
+//     }
+//   }
+// )
+
+function Controls() {
+  const inc = useStore(state => state.increments)
+  return <button onClick={inc}>Increments</button>
+}
+
+function Counter() {
+  const count = useStore(state => state.count)
+  return <h1>{count}</h1>
+}
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Counter />
+      <Controls />
     </div>
   )
 }
 
 export default App
+
+
+// const useStore = function create(set){
+//   return {
+//     count : 1,
+//     inc : function() {
+//       return set (function(state){
+//         return {
+//           count : state.count + 1
+//         }
+//       })
+//     }
+//   }
+// }
